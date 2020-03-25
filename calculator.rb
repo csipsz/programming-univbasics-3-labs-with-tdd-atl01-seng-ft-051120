d61ddaebe0e8be0107df8a6faf72930f18e20b64
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-25 20:45:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 7
 second_number = 9
 sum = first_number + second_number
-difference = first_number - second_number
+difference = 7 - 9
 product = first_number * second_number
 quotient = first_number / second_number
